4021e9c2938e5533b010a1a69b093b6ff22b9210
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-01-22 08:13:48 +0100

    Pre-trained models with better prompts

--- run_pretrained.py
+++ run_pretrained.py
@@ -5,38 +5,35 @@ from sklearn.model_selection import train_test_split
 from transformers import AutoModelForMaskedLM, AutoTokenizer
 from datasets import load_dataset
 
-from graph_dataset import GraphDataset
 
-LIST_LOCATION = './data/lists_2nodes.txt'
-RESULTS_LOCATION = './data/results_2nodes.txt'
+from transformers import pipeline
+
+
 MODEL_BASE = 'distilbert-base-uncased'
+PROMPT = "a normalized synchronous data flow graph with weights (2,3) needs [MASK] tokens for liveness" 
+
+classifier = pipeline("fill-mask", model = MODEL_BASE)
+
+print(classifier(PROMPT))
 
 
-model = AutoModelForMaskedLM.from_pretrained(MODEL_BASE)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_BASE)
+"""
+Prompt:
+ a normalized synchronous data flow graph with weights (2,3) needs [MASK] tokens for liveness
 
-# lists = open(LIST_LOCATION, 'r').readlines()
-# tokens = open(RESULTS_LOCATION, 'r').readlines()
-list_dataset = load_dataset("text", data_files=LIST_LOCATION)
+Answers:
 
-def tokenize_function(examples):
-    result = tokenizer(examples)
-    if tokenizer.is_fast:
-        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
-    return result
+Distilbert-base-uncased:
+1. matching
+2. random
+3. additional
+4. appropriate
+5. different
 
-tokens = tokenize_function(lists)
-# print(tokens)
-
-
-# Use batched=True to activate fast multithreading!
-# tokenized_datasets = imdb_dataset.map(
-    # tokenize_function, batched=True, remove_columns=["text", "label"]
-# )
-# tokenized_datasets
-# def readInput(lists, tokens):
-    # return lists, tokens
-
-# def getEncodings():
-    # lists, tokens = readInput(LIST_LOCATION, RESULTS_LOCATION)
-    # train_lists, val_lists, train_tokens, val_tokens = train_test_split(lists, tokens, test_size=.2)
+Distilroberta-base
+1. 2048
+2. additional
+3. multiple
+4. validation
+5. 5
+"""
