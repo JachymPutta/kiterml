b734865d23366ffac73320f6c8bed1c3de41a9e0
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-01-26 09:38:00 +0100

    Generic neural net script

---
+++ run_nn.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+from torch import Tensor
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+DATA_LOCATION = './data/data3node.txt'
+LIST_LOCATION = './data/lists_3nodes.txt'
+RESULTS_LOCATION = './data/results_3nodes.txt'
+
+def preprocess():
+    data = []
+    results = []
+
+    with open(LIST_LOCATION) as listFile:
+        with open(RESULTS_LOCATION) as resFile:
+            for lis, res in zip(listFile,resFile):
+                intList = list(map(int, lis[:-1].split(',')))
+
+                results.append(int(res[:-1]))
+                data.append(intList)
+
+    return pd.DataFrame(data), pd.DataFrame(results)
+
+data, res = preprocess()
+scaler = StandardScaler()
+model = MLPClassifier()
+
+dat_trn, dat_tst, res_trn, res_tst = train_test_split(data, res, test_size=0.2, random_state=42)
+
+model.fit(dat_trn, res_trn.iloc[:,0])
+
+acc_trn = accuracy_score(res_trn.iloc[:,0], model.predict(dat_trn))
+acc_tst = accuracy_score(res_tst.iloc[:,0], model.predict(dat_tst))
+
+print("Training accuracy is: %.2f" %(acc_trn))
+print("Test accuracy is: %.2f" %(acc_tst))
+
+# Training accuracy: 0.28
+# Test accuracy: 0.24
