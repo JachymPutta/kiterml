c9189b4166fcdfdc507e9e569813e07a3bdf61f7
Jachym <jachym.putta@u.yale-nus.edu.sg>
2023-04-09 17:45:19 +0800

    Reading evals from files

--- constants.py
+++ constants.py
@@ -9,13 +9,13 @@ TO_FILE = True
 # Directories
 OUTPUT_FILE = 'result.tmp'
 FIG_DIR = 'figs/'
-DATA_LOCATION = 'data/data2node.txt'
+DATA_LOCATION = 'data/data4node.txt'
 
 # Model Parameters
-MULT_FACTOR = [1,2]
-DUP_FACTOR = 3
+MULT_FACTOR = [1,2,3,4]
+DUP_FACTOR = 1
 TRAIN_SET_PERCENTAGE = [15, 35, 55, 75, 95]
-GRAPH_SIZE = 2
+GRAPH_SIZE = 4
 RANDOM_SEED = 42
 
 #Pretrained model defaults
--- main.py
+++ main.py
@@ -13,24 +13,31 @@ from utils.eval_utils import run_eval, run_eval_iter, eval_model
 x_train, x_test, y_train, y_test = preprocess()
 
 # Training all the models
-evals_tf = run_eval(TF_MODEL,  x_train, x_test, y_train, y_test)
-evals_sklearn = run_eval(SKLEARN_MODEL, x_train, x_test, y_train, y_test)
+# evals_tf = run_eval(TF_MODEL,  x_train, x_test, y_train, y_test)
+# evals_sklearn = run_eval(SKLEARN_MODEL, x_train, x_test, y_train, y_test)
 
-print(evals_tf)
-print(evals_sklearn)
 # Record results -- only for iterative training
-write_results(TF_MODEL + "_" + OUTPUT_FILE, evals_tf)
-write_results(SKLEARN_MODEL + "_" + OUTPUT_FILE, evals_sklearn)
+# write_results(TF_MODEL + "_" + OUTPUT_FILE, evals_tf)
+# write_results(SKLEARN_MODEL + "_" + OUTPUT_FILE, evals_sklearn)
 
 # Plotting
-plot_loss_curve(TF_MODEL, evals_tf['train_val_loss'])
-plot_loss_curve(SKLEARN_MODEL, evals_sklearn['train_val_loss'])
+# plot_loss_curve(TF_MODEL, evals_tf['train_val_loss'])
+# plot_loss_curve(SKLEARN_MODEL, evals_sklearn['train_val_loss'])
 
 # Load saved models
-with open('sklearn_model.pkl', 'rb') as f:
-    sklearn_model = pickle.load(f)
-    eval_model(sklearn_model, x_test, y_test)
-tf_model = tf.keras.models.load_model("tf_model.h5")
-eval_model(tf_model, x_test, y_test)
+# with open('sklearn_model.pkl', 'rb') as f:
+#     sklearn_model = pickle.load(f)
+#     eval_model(sklearn_model, x_test, y_test)
+# tf_model = tf.keras.models.load_model("tf_model.h5")
+# eval_model(tf_model, x_test, y_test)
 
+# Load evals - from file
+#   with open('tf_eval_res.pkl', 'rb') as f:
+#       evals_tf = pickle.load(f)
+#       plot_loss_curve(TF_MODEL, evals_tf['train_val_loss'])
+#       write_results(TF_MODEL + "_" + OUTPUT_FILE, evals_tf)
 
+#   with open('sklearn_eval_res.pkl', 'rb') as f:
+#       evals_tf = pickle.load(f)
+#       plot_loss_curve(SKLEARN_MODEL, evals_sklearn['train_val_loss'])
+#       write_results(SKLEARN_MODEL + "_" + OUTPUT_FILE, evals_sklearn)
