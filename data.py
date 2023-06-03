8e087bae8e8d6cefd788b60013b11826d09b4cdc
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-06-03 13:55:48 +0200

    Graph data generation

--- data.py
+++ data.py
@@ -8,6 +8,7 @@ import os
 import constants
 
 from generation.gen_data import gen_dict
+from generation.gen_gnn import gen_gnn
 from generation.gen_permutation_throughput import gen_permutations
 from generation.gen_min_max_throughut import gen_min_max
 from generation.gen_denormalized import gen_denormalized
@@ -15,7 +16,7 @@ from generation.gen_denormalized import gen_denormalized
 parser = argparse.ArgumentParser(description="KiterML: data set generation tools")
 
 parser.add_argument('--type', help='Type of data to generate',
-                         choices=['dict', 'min_max', 'permutations', 'denormalized'], required=True)
+                         choices=['dict', 'gnn', 'min_max', 'permutations', 'denormalized'], required=True)
 parser.add_argument('--file', help='Input data file - NOT for dict')
 parser.add_argument('--kiter', help='Path to the kiter binary - dict only')
 parser.add_argument('--limit', help='Limit tokens - dict only')
@@ -25,6 +26,8 @@ args = parser.parse_args()
 
 if args.type == "dict":
     gen_dict(int(args.limit), args.kiter)
+elif args.type == "gnn":
+    gen_gnn()
 elif args.type == "min_max":
     gen_min_max(args.file, args.out)
 elif args.type == "permutations":
--- generation/gen_gnn.py
+++ generation/gen_gnn.py
@@ -4,22 +4,27 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 from utils.data_utils import preprocess
-from constants import RANDOM_SEED, GNN_SCHEMA_LOCATION, \
+from constants import RANDOM_SEED, \
     GNN_TRAIN_LOCATION, GNN_VAL_LOCATION, GNN_TEST_LOCATION, GRAPH_SIZE
 
-x, x_test, y, y_test = preprocess()
-x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15, random_state=RANDOM_SEED)
+def gen_gnn():
+    x, x_test, y, y_test = preprocess()
+    x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.15, random_state=RANDOM_SEED)
 
-x_train = x_train.values.tolist()
-x_val = x_val.values.tolist()
-x_test = x_test.values.tolist()
+    x_train = x_train.values.tolist()
+    x_val = x_val.values.tolist()
+    x_test = x_test.values.tolist()
 
-y_train = y_train.values.tolist()
-y_val = y_val.values.tolist()
-y_test = y_test.values.tolist()
+    y_train = y_train.values.tolist()
+    y_val = y_val.values.tolist()
+    y_test = y_test.values.tolist()
 
-graph_schema = tfgnn.read_schema(GNN_SCHEMA_LOCATION)
-gtspec = tfgnn.create_graph_spec_from_schema_pb(graph_schema)
+    # graph_schema = tfgnn.read_schema(GNN_SCHEMA_LOCATION)
+    # gtspec = tfgnn.create_graph_spec_from_schema_pb(graph_schema)
+
+    write_gnns_to_file(x_train, y_train, GRAPH_SIZE, GNN_TRAIN_LOCATION)
+    write_gnns_to_file(x_val, y_val, GRAPH_SIZE, GNN_VAL_LOCATION)
+    write_gnns_to_file(x_test, y_test, GRAPH_SIZE, GNN_TEST_LOCATION)
 
 
 def write_gnns_to_file(x_lst, y_lst, graph_size, file):
@@ -29,6 +34,9 @@ def write_gnns_to_file(x_lst, y_lst, graph_size, file):
     elif graph_size == 4:
         src = tf.constant([0, 1, 2, 3])
         tgt = tf.constant([1, 2, 3, 0])
+    elif graph_size == 8:
+        src = tf.constant([0, 1, 2, 3, 4, 5, 6, 7])
+        tgt = tf.constant([1, 2, 3, 4, 5, 6, 7, 0])
     else:
         raise Exception("write_gnns_to_file: unsupported graph size")
     with tf.io.TFRecordWriter(file) as writer:
@@ -66,6 +74,3 @@ def write_gnns_to_file(x_lst, y_lst, graph_size, file):
             writer.write(example.SerializeToString())
 
 
-write_gnns_to_file(x_train, y_train, GRAPH_SIZE, GNN_TRAIN_LOCATION)
-write_gnns_to_file(x_val, y_val, GRAPH_SIZE, GNN_VAL_LOCATION)
-write_gnns_to_file(x_test, y_test, GRAPH_SIZE, GNN_TEST_LOCATION)
